b84476e8428bf93aeb9b9e421a5b4066a4fcc291
5MI7th3MI6 <68974383+5MI7th3MI6@users.noreply.github.com>
2020-10-16 20:52:24 -0400

    Create 2.2.3: Declaring and initializing variables.java

---
+++ 2 - Variables_Assignments/2.2.3: Declaring and initializing variables.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class DeclaringVariables {
+   public static void main(String[] args) {
+
+
+      int numHouses = 25;
+
+
+      System.out.println(numHouses);
+   }
+}
