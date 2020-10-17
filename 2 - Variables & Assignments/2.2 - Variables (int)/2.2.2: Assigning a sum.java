869521c416d625956996b7d43c184428af5aaeac
5MI7th3MI6 <68974383+5MI7th3MI6@users.noreply.github.com>
2020-10-16 21:02:08 -0400

    Update 2.2.2: Assigning a sum.java

--- 2 - Variables & Assignments/2.2 - Variables (int)/2.2.2: Assigning a sum.java
+++ 2 - Variables & Assignments/2.2 - Variables (int)/2.2.2: Assigning a sum.java
@@ -1,1 +1,18 @@
-test
+import java.util.Scanner;
+
+public class AssigningSum {
+   public static void main(String[] args) {
+      int numCoins;
+      int numNickels;
+      int numDimes;
+
+      numNickels = 5;
+      numDimes = 6;
+     
+      numCoins = numNickels + numDimes; 
+
+      System.out.print("There are ");
+      System.out.print(numCoins);
+      System.out.println(" coins");
+   }
+}
