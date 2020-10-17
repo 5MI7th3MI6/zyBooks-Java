7fcf637e81ea4d8dbd777dbeab703c72e34e42d1
5MI7th3MI6 <68974383+5MI7th3MI6@users.noreply.github.com>
2020-10-16 20:46:39 -0400

    Create  1.4.1: Basic syntax errors.java

---
+++ Introduction to Java/1.4 - Errors and warnings/1.4.1: Basic syntax errors.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Errors {
+   public static void main(String [] args) {
+      int userNum;
+
+      userNum = 5;
+      System.out.println("Predictions are hard.");
+      System.out.print("Especially ");
+      System.out.println("about the future.");
+      System.out.println("Num is: " + userNum);
+   }
+}
