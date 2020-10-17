41e286880694abdd84d6b4c16a9fd6f0f1d08ed0
5MI7th3MI6 <68974383+5MI7th3MI6@users.noreply.github.com>
2020-10-16 21:47:31 -0400

    Create 5.5.3: While loop: Insect growth.java

---
+++ 5 - Loops/5.5 - More while examples/5.5.3: While loop: Insect growth.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class InsectGrowth {
+   public static void main (String [] args) {
+      Scanner scnr = new Scanner(System.in);
+      int numInsects;
+
+      numInsects = scnr.nextInt(); // Must be >= 1
+             
+      while (numInsects >= 1 && numInsects < 200){
+      System.out.print(numInsects + " "); 
+      numInsects = numInsects * 2; 
+      
+      }
+      System.out.println(); 
+   }
+}
