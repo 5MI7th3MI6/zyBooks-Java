4c187fb20f8f2a1ceb6ae87cb574b7adcc5435de
5MI7th3MI6 <68974383+5MI7th3MI6@users.noreply.github.com>
2020-10-16 21:43:54 -0400

    Create 5.4.3: Basic while loop expression.java

---
+++ 5 - Loops/5.4 - While loops/5.4.3: Basic while loop expression.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class DivideByTwoLoop {
+   public static void main (String [] args) {
+      Scanner scnr = new Scanner(System.in);
+      int userNum;
+
+      userNum = scnr.nextInt(); 
+      while (userNum > 1){
+         userNum = userNum / 2; 
+         System.out.print(userNum + " "); 
+      }
+      System.out.println("");
+   }
+}
