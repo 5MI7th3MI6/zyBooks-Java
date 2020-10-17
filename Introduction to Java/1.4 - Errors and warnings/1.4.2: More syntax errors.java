3ad59539d02280478e413cef0ffe9ba8dbba0335
5MI7th3MI6 <68974383+5MI7th3MI6@users.noreply.github.com>
2020-10-16 20:47:13 -0400

    Create 1.4.2: More syntax errors.java

---
+++ Introduction to Java/1.4 - Errors and warnings/1.4.2: More syntax errors.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Errors {
+   public static void main (String [] args) {
+      int songNum;
+
+      songNum = 5; 
+      System.out.println("Num: " + songNum);
+      System.out.println(songNum);
+      System.out.println(songNum + " songs"); 
+   }
+}
