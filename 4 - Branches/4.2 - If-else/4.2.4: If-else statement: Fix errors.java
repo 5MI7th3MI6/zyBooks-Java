e324b8a255dbd3b8cfd1c39b60dcd0f6fe6d7ebf
5MI7th3MI6 <68974383+5MI7th3MI6@users.noreply.github.com>
2020-10-16 21:19:15 -0400

    Create 4.2.4: If-else statement: Fix errors.java

---
+++ 4 - Branches/4.2 - If-else/4.2.4: If-else statement: Fix errors.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class ConvertNegative {
+   public static void main (String [] args) {
+      Scanner scnr = new Scanner(System.in);
+      int userNum;
+
+      userNum = scnr.nextInt();
+
+      /* Your solution goes here  */
+
+   }
+}
