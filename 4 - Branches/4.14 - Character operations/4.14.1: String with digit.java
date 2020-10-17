0723fded7a0b6a8e6549439ba63c5aae35c6992b
5MI7th3MI6 <68974383+5MI7th3MI6@users.noreply.github.com>
2020-10-16 21:30:37 -0400

    Create 4.14.1: String with digit.java

---
+++ 4 - Branches/4.14 - Character operations/4.14.1: String with digit.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CheckingPasscodes {
+   public static void main (String [] args) {
+      Scanner scnr = new Scanner(System.in);
+      boolean hasDigit;
+      String passCode;
+
+      hasDigit = false;
+      passCode = scnr.next();
+
+      if ((Character.isDigit(passCode.charAt(0))) || (Character.isDigit(passCode.charAt(1))) || (Character.isDigit(passCode.charAt(2)))){
+         hasDigit = true; 
+      } 
+
+      if (hasDigit) {
+         System.out.println("Has a digit.");
+      }
+      else {
+         System.out.println("Has no digit.");
+      }
+   }
+}
