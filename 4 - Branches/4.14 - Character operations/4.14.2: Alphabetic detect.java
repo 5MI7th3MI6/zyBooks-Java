5c0e4aa08dd17b37d12f0c2a7da40db41b1c592e
5MI7th3MI6 <68974383+5MI7th3MI6@users.noreply.github.com>
2020-10-16 21:31:35 -0400

    Create 4.14.2: Alphabetic detect.java

---
+++ 4 - Branches/4.14 - Character operations/4.14.2: Alphabetic detect.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class FindAlpha {
+   public static void main (String [] args) {
+      Scanner scnr = new Scanner(System.in);
+      String passCode;
+
+      passCode = scnr.next();
+
+      int alphabetic; 
+      if (Character.isLetter(passCode.charAt(0))){
+         System.out.println("Alphabetic at 0"); 
+      } 
+      if (Character.isLetter(passCode.charAt(1))){
+         System.out.println("Alphabetic at 1"); 
+      } 
+   }
+}
