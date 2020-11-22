fdf8cc9b632cebfae8504cdcdd625aef76cd872a
5MI7th3MI6 <68974383+5MI7th3MI6@users.noreply.github.com>
2020-11-22 03:40:23 -0500

    Create 8.4.4: Array iteration: Sum of excess.java

---
+++ 8 - Arrays/8.4 - Iterating through arrays/8.4.4: Array iteration: Sum of excess.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class SumOfExcess {
+   public static void main (String [] args) {
+      Scanner scnr = new Scanner(System.in);
+      final int NUM_VALS = 4;
+      int[] testGrades = new int[NUM_VALS];
+      int i;
+      int sumExtra = -9999; // Assign sumExtra with 0 before your for loop
+
+      for (i = 0; i < testGrades.length; ++i) {
+         testGrades[i] = scnr.nextInt();
+      }
+
+      /* Your solution goes here  */
+      sumExtra = 0; 
+      for (i = 0; i < testGrades.length; ++i) {
+         if (testGrades[i] > 100) {
+          sumExtra = sumExtra + testGrades[i] - 100; 
+         }  
+      }
+
+      System.out.println("sumExtra: " + sumExtra);
+   }
+}
