fdb9e22572eb5735c56391b82902e1ca93aa0905
5MI7th3MI6 <68974383+5MI7th3MI6@users.noreply.github.com>
2020-11-22 05:42:53 -0500

    Update 2.28.1: LAB: Expression for calories burned during workout.java

--- Labs/2.28.1: LAB: Expression for calories burned during workout.java
+++ Labs/2.28.1: LAB: Expression for calories burned during workout.java
@@ -1,3 +1,24 @@
+/*
+The following equations estimate the calories burned when exercising (source):
+
+Women: Calories = ( (Age x 0.074) — (Weight x 0.05741) + (Heart Rate x 0.4472) — 20.4022 ) x Time / 4.184
+
+Men: Calories = ( (Age x 0.2017) + (Weight x 0.09036) + (Heart Rate x 0.6309) — 55.0969 ) x Time / 4.184
+
+Write a program using inputs age (years), weight (pounds), heart rate (beats per minute), and time (minutes), respectively. Output calories burned for women and men.
+
+Output each floating-point value with two digits after the decimal point, which can be achieved as follows:
+System.out.printf("%.2f", yourValue);
+
+Ex: If the input is:
+
+49 155 148 60
+the output is:
+
+Women: 580.94 calories
+Men: 891.47 calories
+*/
+
 import java.util.Scanner;
 
 public class LabProgram {
